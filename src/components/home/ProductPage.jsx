fd9dd89d4bbca21d6564baf03375bb2d8128c26d
msuryasyahruli <msuryasyahruli12@gmail.com>
2023-09-05 14:41:32 +0700

    update page

--- src/components/home/ProductPage.jsx
+++ src/components/home/ProductPage.jsx
@@ -3,22 +3,38 @@ import stars from "../../assets/image/Rating 5 stars.png";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import getProductAction from "../../config/redux/actions/ProcuctsActions/getProductAction";
-import Pagination from "../pagination/pagination";
+// import Pagination from "../pagination/pagination";
 
 const ProductPage = () => {
-  const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(4);
-
   const { product } = useSelector((state) => state.product);
   useEffect(() => {
     dispatch(getProductAction());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(4);
+
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentPosts = product.slice(firstPostIndex, lastPostIndex);
+  const page = Math.ceil(product.length / postsPerPage)
+  const number = [...Array(page + 1).keys()].slice(1)
+
+  const perPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  const changePage = (id) => {
+    setCurrentPage(id)
+  }
+  const nextPage = () => {
+    if (currentPage !== page) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
 
   return (
     <>
@@ -60,14 +76,31 @@ const ProductPage = () => {
           </div>
         ))}
       </div>
-      <Pagination
+      <nav className="d-flex justify-content-center">
+        <ul className="pagination">
+          <li className="page-item">
+            <div className="btn page-link" onClick={perPage}>Prev</div>
+          </li>
+          {
+            number.map((n,i) => (
+              <li className={`page-item ${currentPage === n ? 'active' : ''}`} key={i}>
+                <div className="btn page-link" onClick={()=>changePage(n)}>{n}</div>
+              </li>
+            ))
+          }
+          <li className="page-item">
+            <div className="btn page-link" onClick={nextPage}>Next</div>
+          </li>
+        </ul>
+      </nav>
+      {/* <Pagination
         totalPosts={product.length}
         postsPerPage={postsPerPage}
         setCurrentPage={setCurrentPage}
         currentPage={currentPage}
-      />
+      /> */}
     </>
   );
-};
+};  
 
 export default ProductPage;
